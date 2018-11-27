297c81fa2035d4a74091cd93a04227b846d657c2
Macckster <35381093+Macckster@users.noreply.github.com>
2018-11-27 16:20:41 +0100

    Added deck handling and shuffling of decks

--- Visualization/PokerNet/Assets/Scripts/Card.cs
+++ Visualization/PokerNet/Assets/Scripts/Card.cs
@@ -8,17 +8,17 @@ public class Card
 {
     public static Dictionary<string, Card> Cards = new Dictionary<string, Card>();
 
+    public static string[] allCards = new string[]
+       {   "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
+           "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s",
+           "8c", "8d", "8h", "8s", "9c", "9d", "9h", "9s", "10c", "10d", "10h",
+           "10s", "Ac", "Ad", "Ah", "As", "Jc", "Jd", "Jh", "Js", "Kc", "Kd",
+           "Kh", "Ks", "Qc", "Qd", "Qh", "Qs"
+       };
+
     public static void LoadCards()
     {
-        string[] cards = new string[]
-        {   "2c", "2d", "2h", "2s", "3c", "3d", "3h", "3s", "4c", "4d", "4h", "4s",
-            "5c", "5d", "5h", "5s", "6c", "6d", "6h", "6s", "7c", "7d", "7h", "7s",
-            "8c", "8d", "8h", "8s", "9c", "9d", "9h", "9s", "10c", "10d", "10h",
-            "10s", "Ac", "Ad", "Ah", "As", "Jc", "Jd", "Jh", "Js", "Kc", "Kd",
-            "Kh", "Ks", "Qc", "Qd", "Qh", "Qs"
-        };
-
-        foreach (string s in cards)
+        foreach (string s in allCards)
         {
             char suit = s.Last();
 
@@ -62,6 +62,34 @@ public class Card
 
             Cards.Add(s, new Card(tempSuit, den, Resources.Load<Sprite>("Cards/" + s)));
         }
+    }
+
+    public static List<Card> GetDeck()
+    {
+        List<Card> deck = new List<Card>();
+
+        foreach (string card in allCards)
+        {
+            deck.Add(Cards[card]);
+        }
+
+        return deck;
+    }
+
+    public static List<Card> Shuffle(List<Card> cards)
+    {
+        System.Random rng = new System.Random();
+
+        int n = cards.Count;
+        while (n > 1)
+        {
+            int k = rng.Next(n--);
+            Card temp = cards[n];
+            cards[n] = cards[k];
+            cards[k] = temp;
+        }
+
+        return cards;
     }
 
     public enum CardSuit
---
+++ Visualization/PokerNet/Assets/Scripts/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Card.LoadCards();
+        UpdatePlayerHand(Card.Cards["2c"], Card.Cards["Ks"]);
+	}
+
+    public static void UpdatePlayerHand(Card a, Card b)
+    {
+        GameObject.Find("Player Card 1").GetComponent<SpriteRenderer>().sprite = a.cardImage;
+        GameObject.Find("Player Card 2").GetComponent<SpriteRenderer>().sprite = b.cardImage;
+    }
+}
